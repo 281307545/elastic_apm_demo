751ef75bce4797b89614972e4fc5f472ac1505e3
vdiLab <281307545@qq.com>
2020-12-30 13:56:45 +0800

    Add backend demo.

---
+++ backend/src/main/java/com/vdilab/elasticapmdemo/ElasticApmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vdilab.elasticapmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticApmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticApmDemoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vdilab/elasticapmdemo/ElasticApmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdilab.elasticapmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticApmDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
